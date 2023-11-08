a719a287b9427d618fbd8d667875cf962c6bc1f3
pascalFormation <formation.petrovic@gmail.com>
2023-11-08 11:03:04 +0100

    modif lien relatif

--- views/login.php
+++ views/login.php
@@ -1,6 +1,6 @@
-
 <!-- login.php -->
-
+<?php  //include(VIEWS.'head.php'); 
+        include('./headDev.php');?>
 <body>
   <div class="wrapper">
     <section class="form login">
@@ -8,23 +8,29 @@
       <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
         <div class="error-text"><?php echo isset($errorText) ? $errorText : ''; ?></div>
         <div class="field input">
-          <label>Email Address</label>
-          <input type="text" name="email" placeholder="Enter your email" required>
+          <label>Adresse Email</label>
+          <input type="text" name="email" placeholder="Entrer votre Email" required>
         </div>
         <div class="field input">
-          <label>Password</label>
-          <input type="password" name="password" placeholder="Enter your password" required>
+          <label>Mot de passe</label>
+          <input type="password" name="password" placeholder="Tapez votre mot de passe" required>
           <i class="fas fa-eye"></i>
+          
+        </div>
+        <div class="link">
+          <a href="">Mot de passe oublié</a>
         </div>
         <div class="field button">
-          <input type="submit" name="submit" value="Continue to Chat">
+          <input type="submit" name="submit" value="Connexion">
         </div>
       </form>
-      <div class="link">Not yet signed up? <a href="signup.php">Signup now</a></div>
+      <div class="link">Pas encore inscrit ?<a href="signup.php">S'inscrire maintenant</a></div>
     </section>
   </div>
   
-  <script src="../assets/js/pass-show-hide.js"></script>
-  <script src="../assets/js/login.js"></script>
+  <script src='<?php echo ASSETS ;?>js/pass-show-hide.js'></script>
+  <script src='<?php echo ASSETS ;?>js/login.js'></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 
 </body>
--- views/signup.php
+++ views/signup.php
@@ -1,1 +1,51 @@
 <!-- signup.php -->
+
+<?php include_once ('./head.php'); ?>
+<body>
+<?php include(VIEWS.'header.php'); ?>
+<main> 
+  <div class="wrapper">
+    <section class="form signup">
+      <header>CCI Covoiturage</header>
+      <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
+        <div class="error-text"></div>
+        <div class="name-details">
+          <div class="field input">
+            <label>First Name</label>
+            <input type="text" name="fname" placeholder="First name" required>
+          </div>
+          <div class="field input">
+            <label>Last Name</label>
+            <input type="text" name="lname" placeholder="Last name" required>
+          </div>
+        </div>
+        <div class="field input">
+          <label>Email Address</label>
+          <input type="text" name="email" placeholder="Enter your email" required>
+        </div>
+        <div class="field input">
+          <label>Password</label>
+          <input type="password" name="password" placeholder="Enter new password" required>
+          <i class="fas fa-eye"></i>
+        </div>
+        <div class="field image">
+          <label>Select Image</label>
+          <input type="file" name="image" accept="image/x-png,image/gif,image/jpeg,image/jpg" required>
+        </div>
+        <div class="field button">
+          <input type="submit" name="submit" value="Continue to Chat">
+        </div>
+      </form>
+      <div class="link">Already signed up? <a href="login.php">Login now</a></div>
+    </section>
+  </div>
+  </main>
+  <?php include(VIEWS.'footer.php') ;?>
+  <script src="../assets/js/pass-show-hide.js"></script>
+  <script src="../assets/js/signup.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
