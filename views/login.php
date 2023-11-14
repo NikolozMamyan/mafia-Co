f4d5fbebf3505cfe12ba713e1cd756a31e29a4c3
formation.petrovic@gmail.com <formation.petrovic@gmail.com>
2023-11-14 11:05:36 +0100

    modif structure all

--- views/login.php
+++ views/login.php
@@ -3,55 +3,48 @@ include('./headDev.php');
 ?>
 
 <body>
-  <main>
-    <div class="container align-items-center ">
-      <div class="position-relative">
-      <img src="../assets/images/Car driving-bro.svg" alt="" class="img-fluid w-100 desktop-image opacity__50">
-        <img src="../assets/images/Background car SVG.svg" alt="" class="img-fluid w-100 mobile-image opacity__50">
-        <div class="position-absolute top-50 start-50 translate-middle ">
+  <main class="d-flex justify-content-center align-items-center">
+    <div class=" container d-flex justify-content-center align-items-center m-0 p-0 destop-image ">
 
+      <div class="row w-50  wrapper__s bg__section--login m-3">
 
-          <div class="row justify-content-center p-5 wrapper__s bg__section--login ">
+        <section class="col-md-5  align-self-center ">
+          <p class="resum p-lg-5">Rejoignez notre communauté de covoitureurs en vous connectant ou en vous inscrivant.</p>
+        </section>
 
-            <section class="col-md-5 d-flex align-items-center">
-              <p class="resum">Rejoignez notre communauté de covoitureurs en vous connectant ou en vous inscrivant.</p>
-            </section>
+         <section class=" col-md-7 p-lg-3">
+         <form class="" action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
 
-            <section class="col-md-7">
-              <form class="" action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
+            <div class="mb-3">
+              <label for="" class="form-label ps-3 label__white ">Adresse Mail</label>
+              <input type="email" class="form-control rounded-pill" id="" aria-describedby="" placeholder="Entrez votre adresse mail" required>
+            </div>
 
-                <div class="mb-3">
-                  <label for="" class="form-label label__white">Adresse Mail</label>
-                  <input type="email" class="form-control rounded-pill" id="" aria-describedby="" placeholder="Entrez votre adresse mail" required>
-                </div>
+            <div class="input-group mb-3">
+              <label for="password" class="form-label ps-3 label__white">Mot de passe</label>
+              <div class="input-group ">
+                <input type="password" class="form-control radius__left" id="password" name="password" placeholder="Password" required>
+                <span class="input-group-text toggle-password radius__right"><i class="fa fa-eye"></i></span>
+              </div>
+              <div class="ps-3">
+                <a href="#">Mot de passe oublié</a>
+              </div>
+            </div>
 
-                <div class="input-group mb-3">
-                  <label for="password" class="form-label label__white">Mot de passe</label>
-                  <div class="input-group ">
-                    <input type="password" class="form-control radius__left" id="password" name="password" placeholder="Password" required>
-                    <span class="input-group-text toggle-password radius__right"><i class="fa fa-eye"></i></span>
-                  </div>
-                  <div class="">
-                    <a href="#">Mot de passe oublié</a>
-                  </div>
-                </div>
-                
-                  <div class="offset-md-6 col-md-6 ms-auto ">
-                    <button type="submit" name="submit" class="btn btn-lg w-100 rounded-pill bg__btn--submit label__white">Connexion</button>
-                  </div>
-                
-                <div class="">
-                  <p class="label__white">Pas encore inscrit ? <a href="signup.php">S'inscrire maintenant</a></p>
-                </div>
+            <div class="offset-md-6 col-md-6 mb-2 ">
+              <button type="submit" name="submit" class="btn btn-md w-100 rounded-pill bg__btn--submit label__white">Connexion</button>
+            </div>
 
-              </form>
-            </section>
+            <div class="offset-md-2 col-md-10">
+              <p class="label__white">Pas encore inscrit ? <a href="signup.php">S'inscrire maintenant</a></p>
+            </div>
 
-          </div>
-          
-        </div>
+          </form>
+        </section> 
+
       </div>
     </div>
+
   </main>
   <!-- environnement js index -->
   <?php //include(VIEWS.'footer.php') ;
--- views/modifySignup.php
+++ views/modifySignup.php
@@ -1,146 +1,143 @@
 <!-- signup.php -->
 
-<?php include_once('./head.php'); ?>
+<?php include_once('./headDev.php'); ?>
 
 <body>
   <?php //include(VIEWS.'header.php'); 
   ?>
-  <main>
-    <div class="container align-items-center justify-content-center">
-      <div class="position-relative">
-      <img src="../assets/images/Car driving-bro.svg" alt="" class="img-fluid w-100 desktop-image opacity__50">
-        <img src="../assets/images/Background car SVG.svg" alt="" class="img-fluid w-100 mobile-image opacity__50">
-        <div class="position-absolute top-50 start-50 translate-middle ">
-          <div class="row justify-content-center p-5 bg__section--lavande wrapper__s  ">
-            <section>
-              <form class="row needs-validation" action="#" method="POST" enctype="multipart/form-data" autocomplete="off" novalidate>
-                <!-- nom -->
-                <div class="col-lg-6">
-                  <label for="validationCustom01" class="form-label">Nom</label>
-                  <input type="text" class="form-control rounded-pill" id="validationCustom01" value="" required>
+  <main class="d-flex justify-content-center align-items-center">
+    <div class=" container d-flex justify-content-center align-items-center m-0 p-0 destop-image ">
+
+      <div class="row w-50 wrapper__s bg__section--lavande">
+
+        <section>
+          <form class="row needs-validation m-4" action="#" method="POST" enctype="multipart/form-data" autocomplete="off" novalidate>
+            <!-- nom -->
+            <div class="col-lg-6 mb-3">
+              <label for="validationCustom01" class="form-label ps-3">Nom</label>
+              <input type="text" class="form-control rounded-pill" id="validationCustom01" value="" required>
+              <span class="invalid-feedback">
+                Le nom est obligatoire !
+              </span>
+            </div>
+            <!-- prenom -->
+            <div class="col-lg-6 mb-3">
+              <label for="validationCustom02" class="form-label ps-3">Prénom</label>
+              <input type="text" class="form-control rounded-pill" id="validationCustom02" value="" required>
+              <span class="invalid-feedback">
+                Le Prénom est obligatoire !
+              </span>
+            </div>
+            <!-- adresse -->
+            <div class="col-lg-6 mb-3">
+              <label for="validationCustom03" class="form-label ps-3">Adresse</label>
+              <input type="text" class="form-control rounded-pill" id="validationCustom03" required>
+              <span class="invalid-feedback">
+                L'adresse est obligatoire !
+              </span>
+            </div>
+            <!-- code postal -->
+            <div class="col-lg-2 mb-3">
+              <label for="validationCustom02" class="form-label ps-3">CP</label>
+              <input type="text" class="form-control rounded-pill" id="validationCustom02" value="" required>
+              <span class="invalid-feedback">
+                Le code postal est obligatoire !
+              </span>
+            </div>
+            <!-- ville -->
+            <div class="col-lg-4 mb-3">
+              <label for="validationCustom02" class="form-label ps-3">Ville</label>
+              <input type="text" class="form-control rounded-pill" id="validationCustom02" value="" required>
+              <span class="invalid-feedback">
+                La ville est obligatoire !
+              </span>
+            </div>
+            <!-- telephone -->
+            <div class="col-lg-6 mb-3">
+              <label for="" class="form-label ps-3">Téléphone</label>
+              <input type="text" class="form-control rounded-pill" id="" value="">
+
+            </div>
+            <!-- mail -->
+            <div class="col-lg-6 mb-3">
+              <label for="validationCustom02" class="form-label ps-3">Mail</label>
+              <input type="text" class="form-control rounded-pill" id="validationCustom02" value="" required>
+              <span class="invalid-feedback">
+                L'adresse email est obligatoire !
+              </span>
+            </div>
+            <!-- mot de passe -->
+            <div class="col-lg-6 mb-3">
+              <div class="input-group ">
+                <label for="password" class="form-label ps-3">Mot de passe</label>
+                <div class="input-group ">
+                  <input type="password" class="form-control radius__left" id="password" name="password" placeholder="Password" required>
+                  <span class="input-group-text toggle-password radius__right"><i class="fa fa-eye"></i></span>
                   <span class="invalid-feedback">
-                    Le nom est obligatoire !
-                  </span>
-                </div>
-                <!-- prenom -->
-                <div class="col-lg-6">
-                  <label for="validationCustom02" class="form-label">Prénom</label>
-                  <input type="text" class="form-control rounded-pill" id="validationCustom02" value="" required>
-                  <span class="invalid-feedback">
-                    Le Prénom est obligatoire !
-                  </span>
-                </div>
-                <!-- adresse -->
-                <div class="col-lg-6">
-                  <label for="validationCustom03" class="form-label">Adresse</label>
-                  <input type="text" class="form-control rounded-pill" id="validationCustom03" required>
-                  <span class="invalid-feedback">
-                    L'adresse est obligatoire !
-                  </span>
-                </div>
-                <!-- code postal -->
-                <div class="col-lg-2">
-                  <label for="validationCustom02" class="form-label">CP</label>
-                  <input type="text" class="form-control rounded-pill" id="validationCustom02" value="" required>
-                  <span class="invalid-feedback">
-                    Le code postal est obligatoire !
-                  </span>
-                </div>
-                <!-- ville -->
-                <div class="col-lg-4">
-                  <label for="validationCustom02" class="form-label">Ville</label>
-                  <input type="text" class="form-control rounded-pill" id="validationCustom02" value="" required>
-                  <span class="invalid-feedback">
-                    La ville est obligatoire !
-                  </span>
-                </div>
-                <!-- telephone -->
-                <div class="col-lg-6">
-                  <label for="" class="form-label rounded-pill">Téléphone</label>
-                  <input type="text" class="form-control" id="" value="" >
-                  
-                </div>
-                <!-- mail -->
-                <div class="col-lg-6">
-                  <label for="validationCustom02" class="form-label">Mail</label>
-                  <input type="text" class="form-control rounded-pill" id="validationCustom02" value="" required>
-                  <span class="invalid-feedback">
-                    L'adresse email est obligatoire !
-                  </span>
-                </div>
-                <!-- mot de passe -->
-                <div class="col-lg-6">
-                  <div class="input-group ">
-                    <label for="password" class="form-label ">Mot de passe</label>
-                    <div class="input-group ">
-                      <input type="password" class="form-control radius__left" id="password" name="password" placeholder="Password" required>
-                      <span class="input-group-text toggle-password radius__right"><i class="fa fa-eye"></i></span>
-                      <span class="invalid-feedback">
                     Le mot de passe est invalide !
                   </span>
-                    </div>
-
-                  </div>
                 </div>
-                <!-- confirm mot de passe -->
-                <div class="col-lg-6">
-                  <div class="input-group ">
-                    <label for="password" class="form-label ">Mot de passe</label>
-                    <div class="input-group ">
-                      <input type="password" class="form-control rounded-pill" id="password" name="password" placeholder="Password" required>
-                      <!-- <span class="input-group-text toggle-password radius__right"><i class="fa fa-eye"></i></span> -->
-                      <span class="invalid-feedback">
+
+              </div>
+            </div>
+            <!-- confirm mot de passe -->
+            <div class="col-lg-6 mb-3">
+              <div class="input-group ">
+                <label for="password" class="form-label ps-3">Mot de passe</label>
+                <div class="input-group ">
+                  <input type="password" class="form-control rounded-pill" id="password" name="password" placeholder="Password" required>
+                  <!-- <span class="input-group-text toggle-password radius__right"><i class="fa fa-eye"></i></span> -->
+                  <span class="invalid-feedback">
                     Le mot de passe est invalide !
                   </span>
-                    </div>
-
-                  </div>
-                </div>
-                
-                <!-- fichier photo -->
-                <div class="col-lg-8">
-                  <label for="" class="form-label">Choisir une photo</label>
-                  <input type="file" class="form-control rounded-pill" aria-label="file example" required>
-                  <!-- <div class="invalid-feedback">Example invalid form file feedback</div> -->
-                </div>
-                <!-- role (radio btn) -->
-                <div class="col-lg-4">
-                
-                  <div class="form-check">
-                
-                    <input type="radio" class="form-check-input" id="validationFormCheck1" name="radio-stacked" required>
-                    <label class="form-check-label" for="validationFormCheck1">Conducteur/Passager</label>
-                    
-                  </div>
-                  <div class="form-check ">
-                    <input type="radio" class="form-check-input" id="validationFormCheck2" name="radio-stacked" required>
-                    <label class="form-check-label" for="validationFormCheck2">Conducteur</label>
-                    
-                  </div>
-                  <div class="form-check ">
-                    <input type="radio" class="form-check-input" id="validationFormCheck3" name="radio-stacked" required>
-                    <label class="form-check-label" for="validationFormCheck3">Passager</label>
-                    <span class="invalid-feedback">
-                    Le choix du role est obligatoire !
-                  </span>
-                  </div>
-                  
-                </div>
-                <!-- btn annuler -->
-                <div class="col-lg-4">
-                  <input class="btn w-100 btn-danger rounded-pill label__white" type="reset" value="Annuler">
-                </div>
-                <!-- btn valider -->
-                <div class="offset-lg-2 col-lg-6">
-                  <button class="btn w-100 rounded-pill bg__btn--submit label__white" type="submit">Modifier</button>
                 </div>
 
-                
-              </form>
+              </div>
+            </div>
+            <!-- role (radio btn) -->
+            <div class="col-lg-12 mb-3">
 
-            </section>
-          </div>
-        </div>
+              <div class="form-check">
+
+                <input type="radio" class="form-check-input" id="validationFormCheck1" name="radio-stacked" required>
+                <label class="form-check-label" for="validationFormCheck1">Conducteur/Passager</label>
+
+              </div>
+              <div class="form-check ">
+                <input type="radio" class="form-check-input" id="validationFormCheck2" name="radio-stacked" required>
+                <label class="form-check-label" for="validationFormCheck2">Conducteur</label>
+
+              </div>
+              <div class="form-check ">
+                <input type="radio" class="form-check-input" id="validationFormCheck3" name="radio-stacked" required>
+                <label class="form-check-label" for="validationFormCheck3">Passager</label>
+                <span class="invalid-feedback">
+                  Le choix du role est obligatoire !
+                </span>
+              </div>
+
+            </div>
+            <!-- fichier photo -->
+            <div class="col-lg-12 mb-3">
+              <label for="" class="form-label ps-3">Choisir une photo</label>
+              <input type="file" class="form-control rounded-pill" aria-label="file example" required>
+              <!-- <div class="invalid-feedback">Example invalid form file feedback</div> -->
+            </div>
+
+            <!-- btn annuler -->
+            <div class="col-lg-4 mb-3">
+              <input class="btn w-100 btn-danger rounded-pill label__white" type="reset" value="Annuler">
+            </div>
+            <!-- btn valider -->
+            <div class="offset-lg-2 col-lg-6 mb-3">
+              <button class="btn w-100 rounded-pill bg__btn--submit label__white" type="submit">Modifier</button>
+            </div>
+
+
+          </form>
+
+        </section>
+
       </div>
     </div>
   </main>
