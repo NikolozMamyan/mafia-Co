8c1f67c034982f26180262cfe7d3f5dcba6b86de
TBW1wnl <alpha.tbw@gmail.com>
2023-11-20 09:02:06 +0100

    profileMap

--- assets/js/leafletMap.js
+++ assets/js/leafletMap.js
@@ -1,5 +1,101 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+let cciLat = 48.5550278925866;
+let cciLon = 7.745056596296454;
+let myLat = 48.43017778308778;
+let myLon = 7.657688465425483;
+let radius = 2; //en km
+let radiusM = radius * 1000; // em metres
+
+// initialize map
+let map = L.map("map", { zoomControl: false }).setView([myLat, myLon], 13);
+const layer = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
+
+// zoom controls on top-right
+const control = L.control
+  .zoom({
+    position: "topright",
+  })
+  .addTo(map);
+
+// waypoint on CCI
+const marker = L.marker([myLat, myLon]).addTo(map);
+
+//route
+// const routing = L.Routing.control({
+//   addWaypoints: false,
+//   draggableWaypoints: false,
+//   waypoints: [L.latLng(cciLat, cciLon), L.latLng(cciLat, cciLon + 1)], //L.latLng(cciLat, cciLon)
+//   routeWhileDragging: true,
+// }).addTo(map);
+
+// //popup CCI
+// const popup = L.popup()
+//   .setLatLng([cciLat, cciLon])
+//   .setContent("CCI Campus.")
+//   .openOn(map);
+
+// const circle = L.circle([myLat, myLon], {
+//   color: "purple",
+//   fillColor: "purple",
+//   fillOpacity: 0.2,
+//   radius: radiusM,
+// }).addTo(map);
+
+const routing = L.Routing.control({
+  addWaypoints: false,
+  draggableWaypoints: false,
+  waypoints: [L.latLng(cciLat, cciLon), L.latLng(cciLat, cciLon + 1)],
+  routeWhileDragging: true,
+});
+
+const circle = L.circle([myLat, myLon], {
+  color: "purple",
+  fillColor: "purple",
+  fillOpacity: 0.2,
+  radius: radiusM,
+});
+
+let addToMap = L.layerGroup();
+function test(btn) {
+  if (btn === 1) {
+    addToMap = marker;
+  } else if (btn === 2) {
+    addToMap = routing;
+  } else if (btn === 3) {
+    addToMap = circle;
+  }
+
+  console.log(addToMap);
+  return addToMap.addTo(map);
+}
+
+let myLayer = L.layerGroup([marker, routing]);
+
+//let myLayer2 = L.layerGroup([routing]);
+
+const setMapView = (view) => {
+  delLayers();
+  switch (view) {
+    case "Location":
+      locationLayer();
+      break;
+    case "Pinpoint":
+      break;
+    case "Place":
+      break;
+    default:
+      break;
+  }
+};
+
+const delLayers = () => {
+  console.log(myLayer);
+  map.removeLayer(myLayer);
+};
+
+const locationLayer = () => {};
+const pinpointLayer = () => {};
+const placeLayer = () => {};
--- views/profile.php
+++ views/profile.php
@@ -3,19 +3,45 @@
 <body class="container-fluid">
 <?php include('./header.php');
       include('./navBar.php');?>
-    <div class="container row">
-        <div class="col-12 offset-sm-2 col-sm-8">
+    <img class="bgimg" src="../assets/images/Car driving-bro.svg" alt="fond">
+    
+    <main class="container">
+        <div class="bg-P200 p-5">
             <div class="row">
-                <div class="col-12 col-sm-6">
-                    test
+                <div class="col-12 col-sm-6 bg-light p-0">
+                    <div class="row bg-P500 p-2 m-0">
+                        <img class="col-3 profilePic" src="../assets/images/profilTemp.jpg" alt="photo">
+                        <div class="col-9 mt-2">
+                            <p>Conducteur/Passager</p>
+                            <p>John Doe</p>
+                            <p>Strasbourg-67150</p>
+                        </div>
+                    </div>
+                    <p class="text-center text-P500-border bld">Mon Trajet Regulier</p>
+                    <p class="container text-P500">Je pars de Colmar du lundi au vendredi pour me rendre au CCI Campus à Strasbourg. Mes cours commencent à 8h00 et se terminent à 17h.</p>
+                    <p class="text-center bg-P500 bld">Commentaire</p>
+                    <p class="container text-P500">Je suis a l'arret x les matins a 7h00</p>
                 </div>
                 <div class="col-12 col-sm-6">
                     <div id="map"></div>
+                    <div class="text-center">
+                        <button onclick="test(1)" class="mapButton active" id="btn1">
+                            <img src="../assets/images/Location.png">
+                        </button>
+                        <button onclick="test(2)" class="mapButton" id="btn2">
+                            <img src="../assets/images/MapPinpoint.png">
+                        </button>
+                        <button onclick="test(3)" class="mapButton" id="btn3">
+                            <img src="../assets/images/PlaceMarker.png">
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </main>
 
 
+    <script src="../assets/leaflet_files/leaflet/leaflet.js"></script>
+    <script src="../assets/leaflet_files/leaflet-routing/leaflet-routing-machine.js"></script>
     <script src="../assets/js/leafletMap.js"></script>
 </body>
