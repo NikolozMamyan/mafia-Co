fff0fd5c6aa3904603838dfc8009315d50def298
NikolozMamyan <115402171+NikolozMamyan@users.noreply.github.com>
2023-11-26 17:15:07 +0300

    Add files via upload

---
+++ views/contact.php
@@ -0,0 +1,47 @@
+<?php 
+include('./headDev.php');
+?>
+</head>
+<body >
+<?php include_once(__DIR__ . '/header.php'); ?>
+<?php include_once(__DIR__ . '/navBar.php'); ?>
+<img class="bgimg" src="../assets/images/Car driving-bro.svg" alt="fond">
+<section class='body_contact mt-4'>
+<div class="container ">
+    <div class="row">
+        <!-- Colonne Email -->
+        <div class="col-md-4">
+            <div class="contact-info">
+                <h2>Email</h2>
+                <h4>Contactez-nous</h4>
+                <p>Email : contact@exemple.com</p>
+            </div>
+        </div>
+
+        <!-- Colonne Téléphone -->
+        <div class="col-md-4">
+            <div class="contact-info">
+                <h2>Téléphone</h2>
+                <h4>Contactez-nous</h4>
+                <p>Téléphone : +33 1 23 45 67 89</p>
+            </div>
+        </div>
+
+        <!-- Colonne Adresse -->
+        <div class="col-md-4">
+            <div class="contact-info">
+                <h2>Adresse</h2>
+                <h4>Contactez-nous</h4>
+                <p>Adresse : 123 Rue de l'Exemple, Ville, Pays</p>
+            </div>
+        </div>
+    </div>
+</div>
+</section>
+
+
+<?php 
+include('./footer.php');
+?>
+</body>
+
