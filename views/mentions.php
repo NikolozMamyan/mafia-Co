b19a0abd95a8192a47af88949fc8e8e3a1c9edfc
NikolozMamyan <115402171+NikolozMamyan@users.noreply.github.com>
2023-11-26 17:16:30 +0300

    new pages

---
+++ views/mentions.php
@@ -0,0 +1,46 @@
+<?php include('./headDev.php'); ?>
+<body>
+<img class="bgimg" src="../assets/images/Car driving-bro.svg" alt="fond">
+<?php include_once(__DIR__ . '/header.php'); ?>
+<?php include_once(__DIR__ . '/navBar.php'); ?>
+<section class='body_mentions mt-4'>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="mentions-info">
+                    <h2>Mentions Légales</h2>
+                    <h4>Informations légales sur notre site</h4>
+                    <p>Texte de vos mentions légales...</p>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div class="mentions-info">
+                    <h2>Web Design</h2>
+                    <h4>Services de conception web</h4>
+                    <p>Texte sur les services de web design...</p>
+                </div>
+            </div>
+
+            <!-- Colonne Développeur -->
+            <div class="col-md-12">
+                <div class="mentions-info">
+                    <h2>Développeur</h2>
+                    <h4>Services de développement web</h4>
+                    <p>Texte sur les services de développement...</p>
+                </div>
+            </div>
+
+            <!-- Colonne Hébergeur -->
+            <div class="col-md-12">
+                <div class="mentions-info">
+                    <h2>Hébergeur</h2>
+                    <h4>Services d'hébergement web</h4>
+                    <p>Texte sur les services d'hébergement...</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php include('./footer.php'); ?>
+</body>
+</html>
--- views/trajet.php
+++ views/trajet.php
@@ -2,18 +2,25 @@
 include('./headDev.php');
 ?>
 
-<body>
-  <main class="container mt-5 container__menu">
+<body >
+<?php include('./header.php');?>
+<?php include('./navBar.php');?>
+      <img class="bgimg" src="../assets/images/Car driving-bro.svg" alt="fond">
+      <section class='trajet my-5'>
+      <main class="container container__menu ">
     <div class="row">
     <div class="col ms-2">
     <div class="form-group">
-        <label for="depart">Départ</label>
-        <input class="form-control rounded-pill mb-5 inpLg" type="text" id="depart" name="depart">
-    </div>
+    <img src="../assets/images/flagG.svg" class='flag' alt="">
+                    <label for="depart">Départ</label>
+                    <input class="form-control rounded-pill mb-5 inpLg" type="text" id="depart" name="depart">
+                    <img src="../assets/images/fleche.png"  class='fleche'alt="">
+                </div>
     <div class="form-group">
-        <label for="arrive">Arrivé</label>
-        <input class="form-control rounded-pill mb-5 inpLg" type="text" id="arrive" name="arrive">
-    </div>
+    <img src="../assets/images/flagR.svg" class='flag' alt="">
+                    <label for="depart">Départ</label>
+                    <input class="form-control rounded-pill mb-5 inpLg" type="text" id="depart" name="depart">
+                </div>
 </div>
       <div class="col ">
         <div class="container mb-5 p-0 selectH">
@@ -74,4 +81,8 @@ include('./headDev.php');
 </div>
   </section>
 </div>
+      </section>
+      <?php 
+include('./footer.php');
+?>
 </body>
