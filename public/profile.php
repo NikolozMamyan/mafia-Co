6684b8e2d646522c3d28b342b6dc5811a93493f8
TBW1wnl <alpha.tbw@gmail.com>
2023-11-27 11:53:21 +0100

    commit pour push sur la base

--- public/profile.php
+++ public/profile.php
@@ -20,48 +20,75 @@ require_once(__DIR__ . '/../views/headDev.php');
                 </div>
             </div>
         </section>
+        <section class="container-fluid ">
+            <div class="row  ">
+                <!-- titre -->
+                <div class="col text-center my-3 px-0">
+                    <h2 class="bg-P500 pb-1">Mon profile</h2>
+                </div>
+            </div>
+        </section>
+        <section class="d-flex justify-content-center align-items-center mb-5">
+            <div class="container d-flex justify-content-center m-0 p-auto desktop-image-profile">
 
-    <main class="container">
-        <div class="bg-P200 p-3 p-md-5 my-5">
-            <div class="row">
-                <section class="col-12 col-md-6 bg-light p-0">
-                    <div class="row bg-P500 p-2 m-0">
-                        <img class="col-auto profilePic" src="./assets/images/profilTemp.jpg" alt="photo">
-                        <div class="col-auto mt-2">
-                            <p>Conducteur/Passager</p>
-                            <p>John Doe</p>
-                            <p>Strasbourg-67150</p>
+                <div class="container">
+                    <div class="bg-P200 p-3 p-md-5 my-5">
+                        <div class="row">
+                            <section class="col-12 col-md-6 bg-light p-0">
+                                <div class="row bg-P500 p-2 m-0">
+                                    <img class="col-auto profilePic" src="./assets/images/profilTemp.jpg" alt="photo">
+                                    <div class="col-auto mt-2">
+                                        <p>Conducteur/Passager</p>
+                                        <p>John Doe</p>
+                                        <p>Strasbourg-67150</p>
+                                    </div>
+                                </div>
+                                <div class="d-none d-sm-block">
+                                    <p class="text-center text-P500-border bld">Mon Trajet Regulier</p>
+                                    <p class="container text-P500">Je pars de Colmar du lundi au vendredi pour me rendre au CCI Campus à Strasbourg. Mes cours commencent à 8h00 et se terminent à 17h.</p>
+                                    <p class="text-center bg-P500 bld">Commentaire</p>
+                                    <p class="container text-P500">Je suis a l'arret x les matins a 7h00</p>
+                                </div>
+                            </section>
+                            <section class="col-12 col-md-6 p-0 px-md-4">
+                                <div class="map-container">
+                                    <div id="map"></div>
+                                </div>
+                                <div class="text-center">
+                                    <button onclick="setMapView('Location')" class="mapButton active" id="btnLocation">
+                                        <img src="./assets/images/Location.png">
+                                    </button>
+                                    <button onclick="setMapView('Pinpoint')" class="mapButton" id="btnMapPinpoint">
+                                        <img src="./assets/images/MapPinpoint.png">
+                                    </button>
+                                    <button onclick="setMapView('Place')" class="mapButton" id="btnPlaceMarker">
+                                        <img src="./assets/images/PlaceMarker.png">
+                                    </button>
+                                </div>
+                            </section>
                         </div>
                     </div>
-                    <div class="d-none d-sm-block">
-                        <p class="text-center text-P500-border bld">Mon Trajet Regulier</p>
-                        <p class="container text-P500">Je pars de Colmar du lundi au vendredi pour me rendre au CCI Campus à Strasbourg. Mes cours commencent à 8h00 et se terminent à 17h.</p>
-                        <p class="text-center bg-P500 bld">Commentaire</p>
-                        <p class="container text-P500">Je suis a l'arret x les matins a 7h00</p>
-                    </div>
-                </section>
-                <section class="col-12 col-md-6 p-0 px-md-4">
-                    <div class="map-container">
-                        <div id="map"></div>
-                    </div>
-                    <div class="text-center">
-                        <button onclick="setMapView('Location')" class="mapButton active" id="btnLocation">
-                            <img src="./assets/images/Location.png">
-                        </button>
-                        <button onclick="setMapView('Pinpoint')" class="mapButton" id="btnMapPinpoint">
-                            <img src="./assets/images/MapPinpoint.png">
-                        </button>
-                        <button onclick="setMapView('Place')" class="mapButton" id="btnPlaceMarker">
-                            <img src="./assets/images/PlaceMarker.png">
-                        </button>
-                    </div>
-                </section>
+                </div>
+
+
+
             </div>
-        </div>
+        </section>
+
+
+        <!--  -->
     </main>
+
+    <footer>
+        <?php
+        require_once(__DIR__ . '/../views/footer.php');
+        ?>
+    </footer>
 
 
     <script src="./assets/leaflet_files/leaflet/leaflet.js"></script>
     <script src="./assets/leaflet_files/leaflet-routing/leaflet-routing-machine.js"></script>
     <script src="./assets/js/leafletMap.js"></script>
 </body>
+
+</html>
