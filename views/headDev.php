1cb9d927b96551280784fad61322e1c9ac963edb
TBW1wnl <alpha.tbw@gmail.com>
2023-12-04 10:50:55 +0100

    merge

--- views/headDev.php
+++ views/headDev.php
@@ -14,7 +14,12 @@
   <link rel="stylesheet" href='../public/assets/css/main.css'>
   <link rel="stylesheet" href="../public/assets/leaflet_files/leaflet/leaflet.css">
   <link rel="stylesheet" href="../public/assets/leaflet_files/leaflet-routing/leaflet-routing-machine.css" />
-    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script defer
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+            integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
+            crossorigin="anonymous"
+    ></script>
     <script defer src="https://kit.fontawesome.com/d8b676d1d0.js" crossorigin="anonymous"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 
 </head>
--- views/search.php
+++ views/search.php
@@ -1,19 +1,22 @@
             <div class="container-fluid">
-                <div class="container">
-                    <?php
-                    require_once("../views/searchBar.php");
-                    ?>
-                </div>
-                <!-- titre -->
-                <div class="col-12 col-md-8 offset-md-2 col-lg-4 offset-lg-4 text-center my-3 px-0">
-                    <h2 class="bg-P500 pb-1 text-align-center">Résultats</h2>
-                </div>
-                <div class="row w-100 ms-1">
-                    <?php
-                    for ($i = 0; $i < 4; $i++) {
-                        include("../views/notificationCard.php");
-                    }
-                    ?>
-                </div>
+                <div class="row">
+                    <div>
+                        <?php
+                        require_once("../views/searchBar.php");
+                        ?>
+                    </div>
 
+
+                    <!-- titre -->
+                    <div class="col-12 col-md-8 offset-md-2 col-lg-4 offset-lg-4 text-center my-3 px-0">
+                        <h2 class="bg-P500 pb-1">Résultats</h2>
+                    </div>
+                    <div class="row w-100 ms-1">
+                        <?php
+                        for ($i = 0; $i < 4; $i++) {
+                            include("../views/notificationCard.php");
+                        }
+                        ?>
+                    </div>
+                </div>
             </div>
