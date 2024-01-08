cfdf3c9bcfa5849c860f8454740327523f3a1eef
TBW1wnl <alpha.tbw@gmail.com>
2024-01-08 08:52:06 +0100

    creation classe user + trajet

--- models/User.php
+++ models/User.php
@@ -4,4 +4,223 @@ namespace models;
 
 class User extends Model
 {
+    protected int $idUtilisateur;
+    protected string $nomUtilisateur;
+    protected string $prenomUtilisateur;
+    protected string $adresseUtilisateur;
+    protected string $telUtilisateur;
+    protected string $emailUtilisateur;
+    protected string $motDePasseUtilisateur;
+    protected string $photoUtilisateur;
+    protected bool $compteActif;
+    protected string $dateInscriptionUtilisateur;
+    protected string $derniereModificationUtilisateur;
+    protected string $roleUtilisateur;
+    protected string $zipcodeUtilisateur;
+    protected string $villeUtilisateur;
+    protected float $latUtilisateur;
+    protected float $lonUtilisateur;
+
+    // Getter pour idUtilisateur
+    public function getIdUtilisateur()
+    {
+        return $this->idUtilisateur;
+    }
+
+    // Setter pour idUtilisateur
+    public function setIdUtilisateur($idUtilisateur)
+    {
+        $this->idUtilisateur = $idUtilisateur;
+    }
+
+    // Getter pour nomUtilisateur
+    public function getNomUtilisateur()
+    {
+        return $this->nomUtilisateur;
+    }
+
+    // Setter pour nomUtilisateur
+    public function setNomUtilisateur($nomUtilisateur)
+    {
+        $this->nomUtilisateur = $nomUtilisateur;
+    }
+
+    // Getter pour adresseUtilisateur
+    public function getAdresseUtilisateur()
+    {
+        return $this->adresseUtilisateur;
+    }
+
+    // Setter pour adresseUtilisateur
+    public function setAdresseUtilisateur($adresseUtilisateur)
+    {
+        $this->adresseUtilisateur = $adresseUtilisateur;
+    }
+
+    // Getter pour telUtilisateur
+    public function getTelUtilisateur()
+    {
+        return $this->telUtilisateur;
+    }
+
+    // Setter pour telUtilisateur
+    public function setTelUtilisateur($telUtilisateur)
+    {
+        $this->telUtilisateur = $telUtilisateur;
+    }
+
+    // Getter pour emailUtilisateur
+    public function getEmailUtilisateur()
+    {
+        return $this->emailUtilisateur;
+    }
+
+    // Setter pour emailUtilisateur
+    public function setEmailUtilisateur($emailUtilisateur)
+    {
+        $this->emailUtilisateur = $emailUtilisateur;
+    }
+
+    // Getter pour motDePasseUtilisateur
+    public function getMotDePasseUtilisateur()
+    {
+        return $this->motDePasseUtilisateur;
+    }
+
+    // Setter pour motDePasseUtilisateur
+    public function setMotDePasseUtilisateur($motDePasseUtilisateur)
+    {
+        $this->motDePasseUtilisateur = $motDePasseUtilisateur;
+    }
+
+    // Getter pour photoUtilisateur
+    public function getPhotoUtilisateur()
+    {
+        return $this->photoUtilisateur;
+    }
+
+    // Setter pour photoUtilisateur
+    public function setPhotoUtilisateur($photoUtilisateur)
+    {
+        $this->photoUtilisateur = $photoUtilisateur;
+    }
+
+    // Getter pour compteActif
+    public function getCompteActif()
+    {
+        return $this->compteActif;
+    }
+
+    // Setter pour compteActif
+    public function setCompteActif($compteActif)
+    {
+        $this->compteActif = $compteActif;
+    }
+
+    // Getter pour dateInscriptionUtilisateur
+    public function getDateInscriptionUtilisateur()
+    {
+        return $this->dateInscriptionUtilisateur;
+    }
+
+    // Setter pour dateInscriptionUtilisateur
+    public function setDateInscriptionUtilisateur($dateInscriptionUtilisateur)
+    {
+        $this->dateInscriptionUtilisateur = $dateInscriptionUtilisateur;
+    }
+
+    // Getter pour derniereModificationUtilisateur
+    public function getDerniereModificationUtilisateur()
+    {
+        return $this->derniereModificationUtilisateur;
+    }
+
+    // Setter pour derniereModificationUtilisateur
+    public function setDerniereModificationUtilisateur($derniereModificationUtilisateur)
+    {
+        $this->derniereModificationUtilisateur = $derniereModificationUtilisateur;
+    }
+
+    // Getter pour roleUtilisateur
+    public function getRoleUtilisateur()
+    {
+        return $this->roleUtilisateur;
+    }
+
+    // Setter pour roleUtilisateur
+    public function setRoleUtilisateur($roleUtilisateur)
+    {
+        $this->roleUtilisateur = $roleUtilisateur;
+    }
+
+    // Getter pour zipcodeUtilisateur
+    public function getZipcodeUtilisateur()
+    {
+        return $this->zipcodeUtilisateur;
+    }
+
+    // Setter pour zipcodeUtilisateur
+    public function setZipcodeUtilisateur($zipcodeUtilisateur)
+    {
+        $this->zipcodeUtilisateur = $zipcodeUtilisateur;
+    }
+
+    // Getter pour villeUtilisateur
+    public function getVilleUtilisateur()
+    {
+        return $this->villeUtilisateur;
+    }
+
+    // Setter pour villeUtilisateur
+    public function setVilleUtilisateur($villeUtilisateur)
+    {
+        $this->villeUtilisateur = $villeUtilisateur;
+    }
+
+    // Getter pour latUtilisateur
+    public function getLatUtilisateur()
+    {
+        return $this->latUtilisateur;
+    }
+
+    // Setter pour latUtilisateur
+    public function setLatUtilisateur($latUtilisateur)
+    {
+        $this->latUtilisateur = $latUtilisateur;
+    }
+
+    // Getter pour lonUtilisateur
+    public function getLonUtilisateur()
+    {
+        return $this->lonUtilisateur;
+    }
+
+    // Setter pour lonUtilisateur
+    public function setLonUtilisateur($lonUtilisateur)
+    {
+        $this->lonUtilisateur = $lonUtilisateur;
+    }
+
+    // Méthode pour obtenir les données de l'utilisateur sous forme de tableau
+    public function toArray()
+    {
+        return [
+            'idUtilisateur' => $this->idUtilisateur,
+            'nomUtilisateur' => $this->nomUtilisateur,
+            'prenomUtilisateur' => $this->prenomUtilisateur,
+            'adresseUtilisateur' => $this->adresseUtilisateur,
+            'telUtilisateur' => $this->telUtilisateur,
+            'emailUtilisateur' => $this->emailUtilisateur,
+            'motDePasseUtilisateur' => $this->motDePasseUtilisateur,
+            'photoUtilisateur' => $this->photoUtilisateur,
+            'compteActif' => $this->compteActif,
+            'dateInscriptionUtilisateur' => $this->dateInscriptionUtilisateur,
+            'derniereModificationUtilisateur' => $this->derniereModificationUtilisateur,
+            'roleUtilisateur' => $this->roleUtilisateur,
+            'zipcodeUtilisateur' => $this->zipcodeUtilisateur,
+            'villeUtilisateur' => $this->villeUtilisateur,
+            'latUtilisateur' => $this->latUtilisateur,
+            'lonUtilisateur' => $this->lonUtilisateur,
+        ];
+    }
 }
---
+++ models/trajet.php
@@ -0,0 +1,288 @@
+<?php
+
+namespace models;
+
+class Trajet extends Model
+{
+    protected int $idTrajet;
+    protected string $adresseDepart;
+    protected string $adresseArrivee;
+    protected int $jourSemaine;
+    protected string $debutCours;
+    protected string $finCours;
+    protected int $nbrPlaceDispo;
+    protected string $infoComplementaire;
+    protected string $dateCreation;
+    protected string $derniereModificationTrajet;
+    protected string $zipcodeDepart;
+    protected string $villeDepart;
+    protected float $latDepart;
+    protected float $lonDepart;
+    protected string $zipcodeArrivee;
+    protected string $villeArrivee;
+    protected float $latArrivee;
+    protected float $lonArrivee;
+
+
+    // Constructeur
+    public function __construct(
+        $adresseDepart,
+        $adresseArrivee,
+        $jourSemaine,
+        $debutCours,
+        $finCours,
+        $nbrPlaceDispo,
+        $infoComplementaire,
+        $dateCreation,
+        $derniereModificationTrajet,
+        $zipcodeDepart,
+        $villeDepart,
+        $latDepart,
+        $lonDepart,
+        $zipcodeArrivee,
+        $villeArrivee,
+        $latArrivee,
+        $lonArrivee
+    ) {
+        $this->adresseDepart = $adresseDepart;
+        $this->adresseArrivee = $adresseArrivee;
+        $this->jourSemaine = $jourSemaine;
+        $this->debutCours = $debutCours;
+        $this->finCours = $finCours;
+        $this->nbrPlaceDispo = $nbrPlaceDispo;
+        $this->infoComplementaire = $infoComplementaire;
+        $this->dateCreation = $dateCreation;
+        $this->derniereModificationTrajet = $derniereModificationTrajet;
+        $this->zipcodeDepart = $zipcodeDepart;
+        $this->villeDepart = $villeDepart;
+        $this->latDepart = $latDepart;
+        $this->lonDepart = $lonDepart;
+        $this->zipcodeArrivee = $zipcodeArrivee;
+        $this->villeArrivee = $villeArrivee;
+        $this->latArrivee = $latArrivee;
+        $this->lonArrivee = $lonArrivee;
+    }
+
+    // Getter et Setter pour idTrajet
+    public function getIdTrajet()
+    {
+        return $this->idTrajet;
+    }
+
+    public function setIdTrajet($idTrajet)
+    {
+        $this->idTrajet = $idTrajet;
+    }
+
+    // Getter et Setter pour adresseDepart
+    public function getAdresseDepart()
+    {
+        return $this->adresseDepart;
+    }
+
+    public function setAdresseDepart($adresseDepart)
+    {
+        $this->adresseDepart = $adresseDepart;
+    }
+
+    // Getter et Setter pour adresseArrivee
+    public function getAdresseArrivee()
+    {
+        return $this->adresseArrivee;
+    }
+
+    public function setAdresseArrivee($adresseArrivee)
+    {
+        $this->adresseArrivee = $adresseArrivee;
+    }
+
+    // Getter et Setter pour jourSemaine
+    public function getJourSemaine()
+    {
+        return $this->jourSemaine;
+    }
+
+    public function setJourSemaine($jourSemaine)
+    {
+        $this->jourSemaine = $jourSemaine;
+    }
+
+    // Getter et Setter pour debutCours
+    public function getDebutCours()
+    {
+        return $this->debutCours;
+    }
+
+    public function setDebutCours($debutCours)
+    {
+        $this->debutCours = $debutCours;
+    }
+
+    // Getter et Setter pour finCours
+    public function getFinCours()
+    {
+        return $this->finCours;
+    }
+
+    public function setFinCours($finCours)
+    {
+        $this->finCours = $finCours;
+    }
+
+    // Getter et Setter pour nbrPlaceDispo
+    public function getNbrPlaceDispo()
+    {
+        return $this->nbrPlaceDispo;
+    }
+
+    public function setNbrPlaceDispo($nbrPlaceDispo)
+    {
+        $this->nbrPlaceDispo = $nbrPlaceDispo;
+    }
+
+    // Getter et Setter pour infoComplementaire
+    public function getInfoComplementaire()
+    {
+        return $this->infoComplementaire;
+    }
+
+    public function setInfoComplementaire($infoComplementaire)
+    {
+        $this->infoComplementaire = $infoComplementaire;
+    }
+
+    // Getter et Setter pour dateCreation
+    public function getDateCreation()
+    {
+        return $this->dateCreation;
+    }
+
+    public function setDateCreation($dateCreation)
+    {
+        $this->dateCreation = $dateCreation;
+    }
+
+    // Getter et Setter pour derniereModificationTrajet
+    public function getDerniereModificationTrajet()
+    {
+        return $this->derniereModificationTrajet;
+    }
+
+    public function setDerniereModificationTrajet($derniereModificationTrajet)
+    {
+        $this->derniereModificationTrajet = $derniereModificationTrajet;
+    }
+
+    // Getter et Setter pour zipcodeDepart
+    public function getZipcodeDepart()
+    {
+        return $this->zipcodeDepart;
+    }
+
+    public function setZipcodeDepart($zipcodeDepart)
+    {
+        $this->zipcodeDepart = $zipcodeDepart;
+    }
+
+    // Getter et Setter pour villeDepart
+    public function getVilleDepart()
+    {
+        return $this->villeDepart;
+    }
+
+    public function setVilleDepart($villeDepart)
+    {
+        $this->villeDepart = $villeDepart;
+    }
+
+    // Getter et Setter pour latDepart
+    public function getLatDepart()
+    {
+        return $this->latDepart;
+    }
+
+    public function setLatDepart($latDepart)
+    {
+        $this->latDepart = $latDepart;
+    }
+
+    // Getter et Setter pour lonDepart
+    public function getLonDepart()
+    {
+        return $this->lonDepart;
+    }
+
+    public function setLonDepart($lonDepart)
+    {
+        $this->lonDepart = $lonDepart;
+    }
+
+    // Getter et Setter pour zipcodeArrivee
+    public function getZipcodeArrivee()
+    {
+        return $this->zipcodeArrivee;
+    }
+
+    public function setZipcodeArrivee($zipcodeArrivee)
+    {
+        $this->zipcodeArrivee = $zipcodeArrivee;
+    }
+
+    // Getter et Setter pour villeArrivee
+    public function getVilleArrivee()
+    {
+        return $this->villeArrivee;
+    }
+
+    public function setVilleArrivee($villeArrivee)
+    {
+        $this->villeArrivee = $villeArrivee;
+    }
+
+    // Getter et Setter pour latArrivee
+    public function getLatArrivee()
+    {
+        return $this->latArrivee;
+    }
+
+    public function setLatArrivee($latArrivee)
+    {
+        $this->latArrivee = $latArrivee;
+    }
+
+    // Getter et Setter pour lonArrivee
+    public function getLonArrivee()
+    {
+        return $this->lonArrivee;
+    }
+
+    public function setLonArrivee($lonArrivee)
+    {
+        $this->lonArrivee = $lonArrivee;
+    }
+
+    // Méthode pour obtenir les données sous forme de tableau
+    public function toArray()
+    {
+        return [
+            'idTrajet' => $this->idTrajet,
+            'adresseDepart' => $this->adresseDepart,
+            'adresseArrivee' => $this->adresseArrivee,
+            'jourSemaine' => $this->jourSemaine,
+            'debutCours' => $this->debutCours,
+            'finCours' => $this->finCours,
+            'nbrPlaceDispo' => $this->nbrPlaceDispo,
+            'infoComplementaire' => $this->infoComplementaire,
+            'dateCreation' => $this->dateCreation,
+            'derniereModificationTrajet' => $this->derniereModificationTrajet,
+            'zipcodeDepart' => $this->zipcodeDepart,
+            'villeDepart' => $this->villeDepart,
+            'latDepart' => $this->latDepart,
+            'lonDepart' => $this->lonDepart,
+            'zipcodeArrivee' => $this->zipcodeArrivee,
+            'villeArrivee' => $this->villeArrivee,
+            'latArrivee' => $this->latArrivee,
+            'lonArrivee' => $this->lonArrivee,
+        ];
+    }
+}
