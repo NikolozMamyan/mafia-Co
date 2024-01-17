60823119edeedeed80ceb3e0d2c55ccad8a30bfb
pascalFormation <formation.petrovic@gmail.com>
2024-01-17 21:17:46 +0100

    retouche

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,44 +1,102 @@
-// const form = document.querySelector(".login form"),
-// continueBtn = form.querySelector(".button input"),
-// errorText = form.querySelector(".error-text");
+//login.js
+const form = document.querySelector('form');
+const email = form.email;
+const password = form.password;
+const emailError = document.getElementById('emailError');
+const passwordError = document.getElementById('passwordError');
+const pwdForgotten = document.getElementById('pwdForgotten');
 
-// form.onsubmit = (e)=>{
-//     e.preventDefault();
-// }
+form.addEventListener('input', (e) => {
+    e.preventDefault();
 
-// continueBtn.onclick = ()=>{
-//     let xhr = new XMLHttpRequest();
-//     xhr.open("POST", "php/login.php", true);
-//     xhr.onload = ()=>{
-//       if(xhr.readyState === XMLHttpRequest.DONE){
-//           if(xhr.status === 200){
-//               let data = xhr.response;
-//               if(data === "success"){
-//                 location.href = "users.php";
-//               }else{
-//                 errorText.style.display = "block";
-//                 errorText.textContent = data;
-//               }
-//           }
-//       }
-//     }
-//     let formData = new FormData(form);
-//     xhr.send(formData);
-// }
+    const inputValue = email.value.trim();
+    const emailRegex = /^[^\s@]+@ccicampus\.fr$/;
 
-// document.addEventListener('DOMContentLoaded', function() {
+    if (inputValue.length >= 2) {
+        if (emailRegex.test(inputValue)) {
+            email.style.borderColor = "green";
+            email.placeholder = "";
+        } else {
+            email.style.borderColor = "red";
+            email.placeholder = "Exemple@ccicampus.fr";
+        }
+    } else {
+        email.style.borderColor = "";
+        email.placeholder = "Entrez votre email";
+    }
+});
 
-//   // provisoire bouton de redirection vers profile
-//   const formulaireLogin = document.querySelector('.form__login');
+form.addEventListener('input', (e) => {
+    e.preventDefault();
 
-//   if (formulaireLogin) {
+    const inputValue = password.value.trim();
+    const passwordRegex = /^(?=(.*[A-Z]){2})(?=(.*[a-z]){2})(?=(.*\d){2})(?=(.*[\W_]){2})[\S]{12,}$/;
 
-//     formulaireLogin.addEventListener('submit', function(event) {
-//           // Empêcher le comportement par défaut du formulaire (éviter une soumission normale)
-//           event.preventDefault();
+    if (inputValue.length >= 2) {
+        if (passwordRegex.test(inputValue)) {
+            password.style.borderColor = "green";
+            password.placeholder = "";
+        } else {
+            password.style.borderColor = "red";
+            password.placeholder = "ex : 12ABcd!#";
+        }
+    } else {
+        password.style.borderColor = "";
+        password.placeholder = "Entrez votre mot de passe";
+    }
+});
 
-//           // Redirection après la soumission du formulaire
-//           window.location.href = '../public/profile.php';
-//       });
-//   }
-// });
+form.addEventListener('submit', (e) => {
+    if (email.value === '') {
+        emailError.textContent = 'L\'email est requis';
+    } else {
+        emailError.textContent = '';
+    }
+
+    passwordError.textContent = '';
+
+    if (password.value === '') {
+        passwordError.textContent = 'Le mot de passe est requis';
+    } else if (password.value.length < 12) {
+        passwordError.textContent = 'Le mot de passe doit comporter au moins 12 caractères';
+    } else if (password.value.length > 25) {
+        passwordError.textContent = 'Le mot de passe doit comporter moins de 25 caractères';
+    }
+
+    if (emailError.textContent === '' && passwordError.textContent === '') {
+        form.submit();
+    }
+});
+pwdForgotten.addEventListener('click', (e) => {
+    e.preventDefault();
+    showModal();
+  });
+
+
+function showModal() {
+    const modal = document.createElement('div');
+    modal.className = 'modal';
+    modal.innerHTML = '<p>un nouveau mot de passe provisoir vous a été envoyé dans votre boite mail</p>';
+
+    document.body.appendChild(modal);
+    setTimeout(function () {
+      modal.remove();
+    }, 30000);
+  }
+  $(document).ready(function() {
+    $("#show_hide_password a").on('click', function(event) {
+        event.preventDefault();
+        if($('#show_hide_password input').attr("type") == "text"){
+            $('#show_hide_password input').attr('type', 'password');
+            $('#show_hide_password i').addClass( "fa-eye-slash" );
+            $('#show_hide_password i').removeClass( "fa-eye" );
+        }else if($('#show_hide_password input').attr("type") == "password"){
+            $('#show_hide_password input').attr('type', 'text');
+            $('#show_hide_password i').removeClass( "fa-eye-slash" );
+            $('#show_hide_password i').addClass( "fa-eye" );
+        }
+    });
+});
+
+
+
--- views/auth/login.php
+++ views/auth/login.php
@@ -1,7 +1,6 @@
 <!-- login.php -->
 <?php require_once base_path('views/components/headDev.php'); ?>
 
-
 <body id="landing-page">
 
   <!-- Partie écran de gauche -->
@@ -17,20 +16,27 @@
 
       <form class="form__login" action="<?php routeEcho('login.check'); ?>" method="POST" enctype="multipart/form-data" autocomplete="off">
         <input type="text" name="action" value="check" hidden>
+        <div class="form-group">
         <label for="login">Votre adresse email
           <input type="email" name="login" placeholder="Entrez ici votre adresse email" />
+          <span class="emailError"></span>
         </label>
+        </div>
+        <div class="form-group">
         <label for="password">Votre mot de passe
-          <a href="">Mot de passe oublié ?</a>
+          <a href="#" class="pwdForgotten">Mot de passe oublié ?</a>
           <input type="password" name="password" placeholder="Entrez ici votre mot de passe" />
-          <button id="show-hide-password"><i class="covoiturage-eye"></i></button>
+          <span id="show-hide-password"><i class="covoiturage-eye"></i></span>
+          <span class="passwordError"></span>
         </label>
+        </div>
+        
         <input type="submit" name="submit" value="Connexion" />
       </form>
     </main>
 
     <footer>
-      <a href="">Conditions générales d'utilisation</a> • <a href="">Mentions légales</a> • © CCI Covoiturage 2023
+      <a href="<?php routeEcho('conditionIndex'); ?>">Conditions générales d'utilisation</a> • <a href="<?php routeEcho('mentionIndex'); ?>">Mentions légales</a> • © CCI Covoiturage 2023
     </footer>
 
   </section>
@@ -39,7 +45,10 @@
   <section id="col-droite" class="desktop-only">
     <img src="assets/images/covoiturage-cci-campus-alsace-illustration-covoiturage-people.svg" alt="illustration personnages en covoiturage" />
   </section>
+
   <!-- js -->
-  <script src="assets/js/pass-show-hide.js"></script>
-  <script src="assets/js/login.js"></script>
+  <script src="./assets/js/pass-show-hide.js"></script>
+  <script src="./assets/js/login.js"></script>
+
+
 </body>
